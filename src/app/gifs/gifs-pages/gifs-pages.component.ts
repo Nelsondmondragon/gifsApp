5ff0219d32176aadcfa04155c03499bea2fcfb45
Nelson Cortes <nelsondmondragon@gmail.com>
2021-12-30 12:46:20 -0500

    Seccion 7: App Gifs

---
+++ src/app/gifs/gifs-pages/gifs-pages.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gifs-pages',
+  templateUrl: './gifs-pages.component.html',
+  styleUrls: ['./gifs-pages.component.css']
+})
+export class GifsPagesComponent {
+
+
+  constructor() { }
+
+}
